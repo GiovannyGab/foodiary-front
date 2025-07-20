02b3f641035a5d1a82befea44013cccfd1a63e5b
GiovannyG <giovanny.gabrieldsn@gmail.com>
2025-07-20 18:48:51 -0300

    feat: adding sign-in and sign up components

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,15 +1,53 @@
-import { createContext } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { createContext, useCallback } from "react";
+import axios from "axios";
+type SignInParams = {
+  email: string;
+  password: string;
+};
+
+type SignUpParams = {
+  goal: string;
+  gender: string;
+  birthDate: string;
+  activityLevel: number;
+  height: number;
+  weight: number;
+  account: {
+    name: string;
+    email: string;
+    password: string;
+  };
+};
 
 interface IAuthContextValue {
   isLoggedIn: boolean;
   isLoading: boolean;
+  signIn(params: SignInParams): Promise<void>;
+  signUp(params: SignUpParams): Promise<void>;
 }
 
 export const AuthContext = createContext({} as IAuthContextValue);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const { mutateAsync: signIn } = useMutation({
+    mutationFn: async (params: SignInParams) => {
+      axios;
+    },
+  });
+  const { mutateAsync: signUp } = useMutation({
+    mutationFn: async (params: SignUpParams) => {},
+  });
+
   return (
-    <AuthContext.Provider value={{ isLoggedIn: false, isLoading: false }}>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: false,
+        isLoading: false,
+        signIn,
+        signUp,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
